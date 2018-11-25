226b8f097ee4e4596cc69371d922bdc15ec5ae49
Md. Shaiful Islam <islambuet@gmail.com>
2018-11-26 04:28:47 +0600

    Removed Transfer controller and test controller Fixed

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -5,11 +5,6 @@ class Test extends CI_Controller
 {
     public function index()
     {
-        $this->load->helper('mobile_sms');
-        $this->lang->load('mobile_sms');
-        echo '<pre>';
-        print_r(Mobile_sms_helper::send_sms(Mobile_sms_helper::$API_SENDER_ID_MALIK_SEEDS,'01713090962',sprintf($this->lang->line('SMS_SALES_INVOICE'),'2,050,000','I-0039107')));
-        echo '</pre>';
 
     }
 }
