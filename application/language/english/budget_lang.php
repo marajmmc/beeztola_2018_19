8123fac9c1062f6909b469af8357dba6ccf169e2
Md. Shaiful Islam <islambuet@gmail.com>
2018-12-08 21:37:53 +0600

    Details included

--- application/language/english/budget_lang.php
+++ application/language/english/budget_lang.php
@@ -4,3 +4,16 @@ $lang['LABEL_DEALER_NAME']='Dealer';
 $lang['LABEL_QUANTITY_BUDGET_KG'] = 'Budget Qtn(kg)';
 $lang['LABEL_STATUS_BUDGET_FORWARD'] = 'Budget Status';
 $lang['LABEL_REVISION_COUNT_BUDGET'] = 'Budgeted time(s)';
+
+// REPORT VARIETY WISE BUDGET TARGET
+$lang['LABEL_PRICE_UNIT_KG_AMOUNT'] = 'Unit Price(kg)';
+$lang['LABEL_BUDGET_KG'] = 'Budget (kg)';
+$lang['LABEL_BUDGET_AMOUNT'] = 'Budget (Amount)';
+$lang['LABEL_TARGET_KG'] = 'Target (kg)';
+$lang['LABEL_TARGET_AMOUNT'] = 'Target (Amount)';
+$lang['LABEL_PREDICTION_KG'] = 'Prediction (kg)';
+$lang['LABEL_PREDICTION_AMOUNT'] = 'Prediction (Amount)';
+$lang['LABEL_BUDGET_SUB_KG'] = 'Sub Budget (Kg)';
+$lang['LABEL_BUDGET_SUB_AMOUNT'] = 'Sub Budget (Amount)';
+$lang['LABEL_TARGET_SUB_KG'] = 'Sub Target (Kg)';
+$lang['LABEL_TARGET_SUB_AMOUNT'] = 'Sub Target (Amount)';
